30da079912f4e3e2fb8a64f81119880496b0ab55
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-11-12 18:09:05 -0500

    Updated app with alpha level inputs

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ source("power_fun.R")
 ui <- fluidPage(
   sliderInput("Power_level", "Power Level", min = .05, max = .95, step = .05, value = c(.5, .95)),
   sliderInput("Cohens_d", "Cohen's D", min = .05, max = 1, step = .05, value = c(.5, 1)),
+  numericInput("alpha", "Significance Level", min = .01, max = 1, step = .01, value = .05),
   plotOutput("Power_plot")
 )
 
@@ -13,7 +14,8 @@ server <- function(input, output, session) {
     power_fun(x = input$Power_level[1],
               y = input$Power_level[2],
               w = input$Cohens_d[1],
-              z = input$Cohens_d[2])
+              z = input$Cohens_d[2],
+              a = input$alpha)
   })
   
   output$Power_plot <- renderPlot({ggplot(Power_data(), 
--- power_fun.R
+++ power_fun.R
@@ -1,13 +1,13 @@
 
 library(pwr)
         
-power_fun <- function(x, y, w, z){
+power_fun <- function(x, y, w, z, a){
   power_level <- seq(from = x, to = y, by = .05)
   cohensd_seq <- seq(from = w, to = z, by =.05)
   results.data <- data.frame(d = numeric(), power = numeric(), n = numeric())
   for (i in cohensd_seq) {
     for (j in power_level) {
-      test <- pwr.t.test(d = i, sig.level = .05, power = j, type = "two.sample",
+      test <- pwr.t.test(d = i, sig.level = a, power = j, type = "two.sample",
                          alternative = "two.sided")
       row <- data.frame(d = i, power = j, n =test$n) 
       results.data <- rbind(results.data, row)
