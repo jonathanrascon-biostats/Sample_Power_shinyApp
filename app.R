2633bc59728d40e12a87e85b7d114c02b6708b86
Jonathan Rascon <jonathan_rascon@brown.edu>
2025-11-16 22:56:03 -0500

    Updated app with input for alpha, assigning d as a factor, updating graphics, and adding a table

--- app.R
+++ app.R
@@ -1,15 +1,22 @@
 library(shiny)
 library(ggplot2)
-library(scales)
+library(bslib)
 source("power_fun.R")
 
-ui <- fluidPage(
-  sliderInput("Power_level", "Power Level", min = .05, max = .95, step = .05, value = c(.5, .95)),
-  sliderInput("Cohens_d", "Cohen's D", min = .05, max = 1, step = .05, value = c(.5, 1)),
-  numericInput("alpha", "Significance Level", min = .01, max = 1, step = .01, value = .05),
-  plotOutput("Power_plot")
+ui <- page_sidebar(
+  title = "Sample Size~Power Relation",
+  sidebar = sidebar(
+    p("This app takes inputs of Power Level, Cohen's D, and Significance Level, 
+       and outputs the appropriate Sample Size needed. It is for a two sample, two-sided T-test, 
+        and uses the pwr.t.test function from the pwr package."),
+    sliderInput("Power_level", "Power Level", min = .05, max = .95, step = .05, value = c(.5, .95)),
+    sliderInput("Cohens_d", "Cohen's D", min = .05, max = 1, step = .05, value = c(.5, 1)),
+    numericInput("alpha", "Significance Level", min = .01, max = 1, step = .01, value = .05),
+    tableOutput("Power_table")
+  ),
+  card(plotOutput("Power_plot"), full_screen = TRUE
+  )
 )
-
 server <- function(input, output, session) {
   Power_data <- reactive({
     power_fun(x = input$Power_level[1],
@@ -20,10 +27,11 @@ server <- function(input, output, session) {
   })
   
   output$Power_plot <- renderPlot({ggplot(Power_data(), 
-        aes(power, n, group = d, color = d)) + geom_line() + scale_y_continuous(n.breaks = 15) +
-      scale_x_continuous(n.breaks = 10) + 
+                                          aes(power, n, group = d, color = d)) + geom_line() + scale_y_continuous(n.breaks = 15) +
+      scale_x_continuous(n.breaks = 10) +
       labs(title = "Power~Sample Relation with Varying D values", x = "Power", y = "Sample Size")
   })
+  output$Power_table <- renderTable({Power_data()})
 }
 
 shinyApp(ui, server)
--- power_fun.R
+++ power_fun.R
@@ -1,6 +1,6 @@
 
 library(pwr)
-        
+
 power_fun <- function(x, y, w, z, a){
   power_level <- seq(from = x, to = y, by = .05)
   cohensd_seq <- seq(from = w, to = z, by =.05)
@@ -9,10 +9,12 @@ power_fun <- function(x, y, w, z, a){
     for (j in power_level) {
       test <- pwr.t.test(d = i, sig.level = a, power = j, type = "two.sample",
                          alternative = "two.sided")
-      row <- data.frame(d = i, power = j, n = test$n) 
+      row <- data.frame(d = i, power = j, n = test$n)
       results.data <- rbind(results.data, row)
     }
   }
+  results.data$n <- round(results.data$n)
+  results.data$d <- factor(results.data$d)
   return(results.data)
 }
 
